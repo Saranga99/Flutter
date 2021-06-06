77a87e010ead2b2aedcb96b58b68a584c48b3b40
saranga thathsara <saranga.2019952@iit.ac.lk>
2021-06-06 07:40:27 +0530

    app_

--- luxury_life/lib/main.dart
+++ luxury_life/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 
+//starting point
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.blueGrey,
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text("Luxury Life"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+            child: Image(
+          image: AssetImage("images/logo.png"),
+        )),
+      ),
     ),
-  ));
+  );
 }
